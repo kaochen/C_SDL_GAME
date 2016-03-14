bdb2fa25f7121b10f9bbc324d6d35ebb06b293ec
kaochen <kaochen2@gmail.com>
2016-03-14 17:40:46 +0100

    two squares in a windowgit status!

--- README.md
+++ README.md
@@ -1,6 +1,33 @@
 # C_SDL_GAME
 C_SDL_Game is a simple SDL Game write to practice C
 
+This is an early draft, use it carefully.
+
+```
 git clone https://github.com/kaochen/C_SDL_GAME
 git remote add -m https://github.com/kaochen/C_SDL_GAME.git
 git remote set-url origin git@github.com:kaochen/C_SDL_GAME.git
+```
+
+### Compile
+```
+sudo apt install libsdl2-2.0-0 libsdl2-dev
+gcc -o game main.c `sdl2-config --cflags --libs`
+./game
+```
+
+### Links
+http://wiki.libsdl.org/Installation#Linux.2FUnix
+
+http://jeux.developpez.com/tutoriels/sdl-2/guide-migration/
+
+http://www.meruvia.fr/index.php/big-tuto-sdl-1/179-big-tuto-sdl-1-2-chapitre-31-conversion-de-projet-vers-la-sdl-2
+
+### Color code :
+```
+127,34,0 (7F2200);
+255,124,76 (FF7C4C);
+255,67,0 (FF4300); background
+127,36,4 (7F2404);
+204,54,0 (CC3600);
+```
---
+++ main.c
@@ -0,0 +1,110 @@
+/*C_SDL_Game is a simple SDL Game write to practice C
+Authors: kao_chen
+Copyright (C) 2016 kao_chen <kaochen2[at]gmail.com>
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <SDL2/SDL.h>
+
+#define GAME_NAME "My Game"
+int w_width = 640;
+int w_height = 480;
+
+void pause();
+
+
+int main(int argc, char *argv[])
+{
+
+  	//check if SDL start correctly
+	if(SDL_Init(SDL_INIT_VIDEO) == -1)
+	  {
+	    fprintf(stderr, "SDL initialization error: %s\n", SDL_GetError());
+	    exit(EXIT_FAILURE);
+	  }
+	    fprintf(stderr, "SDL initialization ok\n");
+
+	//Create the window game
+ 	SDL_Window *window = SDL_CreateWindow(GAME_NAME,
+                          SDL_WINDOWPOS_UNDEFINED,
+                          SDL_WINDOWPOS_UNDEFINED,
+                          w_width,
+			  w_height,
+                          SDL_WINDOW_OPENGL);
+  	//Create a surface form the window
+  	SDL_Surface *screen = NULL;
+  	screen = SDL_GetWindowSurface(window);
+
+  	//Check if the window is created*/
+	if ( window == NULL || screen == NULL )
+   	{
+      	    fprintf(stderr, "Creating the main window failed: %s\n", SDL_GetError());
+	    exit(EXIT_FAILURE);
+    	}
+	    fprintf(stderr, "Creating the main window succeed\n");
+	//initiate surface one
+	SDL_Surface *s = NULL;
+  	SDL_Rect position;
+
+	/* Creating the surface. */
+	s = SDL_CreateRGBSurface(0, 220, 180, 32, 0, 255, 0, 0);
+	/* Filling the surface with red color. */
+	SDL_FillRect(s, NULL, SDL_MapRGB(s->format,127,36,4));
+  	position.x = 10;
+  	position.y = 10;
+	SDL_BlitSurface(s, NULL, screen, &position);
+
+	//initiate surface two
+  	SDL_Surface *s2 = NULL;
+  	SDL_Rect position2;
+
+	/* Creating the surface two. */
+	s2 = SDL_CreateRGBSurface(0, 200, 160, 32, 0, 255, 0, 0);
+	/* Filling the surface with red color. */
+	SDL_FillRect(s2, NULL, SDL_MapRGB(s->format,255, 67, 0 ));
+  	position2.x = 20;
+  	position2.y = 20;
+	SDL_BlitSurface(s2, NULL, screen, &position2);
+
+	//refresh the window
+  	SDL_UpdateWindowSurface(window);
+
+	//wait for quit event
+  	pause();
+  	//clean
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+  	return EXIT_SUCCESS;
+}
+
+
+void pause()
+{
+  int carryOn = 1;
+  SDL_Event event;
+  while(carryOn)
+    {
+      SDL_WaitEvent(&event);
+      switch(event.type)
+	{
+	case SDL_QUIT:
+	  carryOn = 0;
+	}
+    }
+}
+
