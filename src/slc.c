c2ee847eb20af064a63c1ac16eee99a8cd337e3f
kaochen <kaochen2@gmail.com>
2016-09-08 13:30:27 +0200

    forgot locale.h

--- src/slc.c
+++ src/slc.c
@@ -20,6 +20,9 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #ifndef SLC_C
 #define SLC_C
 
+#include <libintl.h>
+#include <locale.h>
+
 #include "../inc/slc.h"
 #include "../inc/menu.h"
 
