587f65cf546356cddfd7f8636033c2fd0ea8cca9
kaochen <kaochen2@gmail.com>
2017-03-10 10:32:21 +0100

    create a README file if necessary

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,15 @@
 #! /bin/sh
+#!/bin/bash
+file="README"
+if [ -f "$file" ]
+then
+	echo "$file found."
+else
+	echo "$file not found. Create a symbolic link between README.md and README "
+	ln  -s README.md README
+fi
 
 aclocal \
+&& autoheader \
 && automake --add-missing \
 && autoconf
