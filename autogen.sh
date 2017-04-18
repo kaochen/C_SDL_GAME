9d5200dd02472564e0e9adb6db08985a37e3001c
kaochen <kaochen2@gmail.com>
2017-04-18 17:23:59 +0200

    fix config.rpath missing

--- autogen.sh
+++ autogen.sh
@@ -8,6 +8,14 @@ else
 	echo "$file not found. Create a symbolic link between README.md and README "
 	ln  -s README.md README
 fi
+file2="config.rpath"
+if [ -f "$file2" ]
+then
+	echo "$file2 found."
+else
+	echo "$file2 not found. Create an empty one"
+	touch config.rpath
+fi
 
 aclocal \
 && autoheader \
