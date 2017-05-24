69f8e83fb035f87425820e77a7ba9940c095e13f
kaochen <kaochen2@gmail.com>
2017-05-24 23:34:29 +0200

    update summary of the files.c

--- src/files.c
+++ src/files.c
@@ -17,7 +17,7 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 
-/*This file handles the reading and decoding of .slc files.*/
+/*Functions to build and manage the list of files that contains the levels.*/
 
 #ifndef FILES_C
 #define FILES_C
